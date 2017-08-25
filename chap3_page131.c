fabd585c1150aced704d9522afdbaf88027c5fb0
Dongfeng Huang <yingfeicaozhang100527@gmail.com>
2017-08-25 18:16:59 +0800

    Update chap3_page131.c

--- chap3_page131.c
+++ chap3_page131.c
@@ -4,20 +4,25 @@
 
 #define LEN 10
 
-void adjust_array(int *arr, int N);
+void adjust_array(int *arr, int N, int (*func)(int));
+int is_even(int num);
 
 int main(){
 
 	int arr[LEN] = {2, 4, 2, 1, 3, 3, 5, 7, 9, 11};
 
-	adjust_array(arr, LEN);
+	adjust_array(arr, LEN, is_even);
 
 	for(int i=0; i<LEN; i++){
 		printf("%d\t", arr[i]);
 	}
 }
 
-void adjust_array(int *arr, int N){
+int is_even(int num){
+	return (num & 1) == 0;
+}
+
+void adjust_array(int *arr, int N, int (*func)(int)){
 	int low = 0;
 	int high = N-1;
 	int temp;
@@ -26,23 +31,15 @@ void adjust_array(int *arr, int N){
 		// find arr[low]%2==0
 		//while(low<high && low<N){
 		// no need "low<N", high acts as upper bound
-		while(low<high){
-			if(arr[low]%2 == 0){
-				break;
-			} else{
-				low++;
-			}
+		while(low<high && !is_even(arr[low])){
+			low++;
 		}
 
 		// find arr[high]%2==1
 		//while(low<high && high>=0){
 		// no need high>0, low acts as lower bound
-		while(low<high){
-			if(arr[high]%2==1){
-				break;
-			}else{
-				high--;
-			}
+		while(low<high && is_even(arr[high])){
+			high--;
 		}
 		if(low < high){
 			temp = arr[low];
