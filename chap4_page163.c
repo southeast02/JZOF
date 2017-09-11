2d453357b16d1ae3e36638ac6179bbb0c0dff0b3
Dongfeng Huang <yingfeicaozhang100527@gmail.com>
2017-09-11 20:48:12 +0800

    Create chap4_page163.c

---
+++ chap4_page163.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_metrix(int** arr, int m, int n);
+
+
+
+int main(){
+	int m;
+	int n;
+	int **arr;
+
+	scanf("%d", &m);
+	scanf("%d", &n);
+
+//	int arr[m][n];
+
+	arr = (int **)malloc(sizeof(int*)*m);
+	for(int i=0; i<m; i++){
+		arr[i] = (int *)malloc(sizeof(int)*n);
+	}
+
+	for(int i=0; i<m; i++){
+		for(int j = 0; j<n; j++)
+			scanf("%d", &arr[i][j]);
+	}
+
+
+	print_metrix(arr, m, n);
+}
+
+
+void print_metrix(int** arr, int m, int n){
+	int round = 0;
+	int i= 0;
+	int j = 0;
+
+	while(i < m && j < n){
+		while(j < n){
+			printf("%d, ", arr[i][j]);
+			j++;
+		}
+
+
+		if (j == n)
+			j--;
+		while(i < m-1){
+			i++;
+			printf("%d, ", arr[i][j]);
+		}
+		n--;//last column has been printed.
+
+		while(j > round && i > round){
+			j--;
+			printf("%d, ", arr[i][j]);
+		}
+
+
+		while(i > round+1 && j < n){
+			i--;
+			printf("%d, ", arr[i][j]);
+		}
+
+		j++;
+		round++;
+		m--;
+	}
+}
+
+
+/**
+ *output
+4 4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16
+1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10,
+
+
+2 3
+1 2 3
+4 5 6
+1, 2, 3, 6, 5, 4,
+
+ 1 4
+1 2 3 4
+1, 2, 3, 4,
+
+4 1
+1 2 3 4
+1, 2, 3, 4,
+ 
+ 1 1
+1
+1, 
+ * */
