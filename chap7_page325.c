52ad73ce364a2a64bcd058da332d455361f0e3a5
Dongfeng Huang <yingfeicaozhang100527@gmail.com>
2017-10-12 16:19:27 +0800

    Update chap7_page325.c

--- chap7_page325.c
+++ chap7_page325.c
@@ -1,25 +1,86 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <limits.h>
 
- int main()
-{
-	printf("INT_MAX: %d\n", 0x7FFFFFFF);
-	printf("INT_MAX: %d\n", INT_MAX);
 
-	puts("");
-	printf("INT_MAX: %d\n", 0x80000000);
-	printf("INT_MAX: %d\n", INT_MIN);
+int atoi_2(const char *str);
+
+
+int exception=0;
+
+int main() {
+	int result;
+	char str[199];
+
+	scanf("%s", str);
+
+	result = atoi_2(str);
+	if(exception == 0){
+		printf("str(%s) = %d\n", str, result);
+	} else {
+		if(exception == -1){
+			printf("wrong input: %s\n", str);
+		} else {
+			printf("overflow!\n");
+		}
+	}
 	return 0;
 }
 
 
-/**
- * output:
-INT_MAX: 2147483647
-INT_MAX: 2147483647
+int atoi_2(const char *str){
+	int flag = 1;
+	long long result = 0; // here result should be long long
+	int str_len = strlen(str);
+	int i=0;
 
-INT_MAX: -2147483648
-INT_MAX: -2147483648
+	if(str_len == 0){
+		exception=-1; // null string
+		return result;
+	} else if(str_len > 11){
+		exception=-2;
+		return result;
+	}
+	if(str[i] == '+'){
+		flag = 1;
+		i++;
+		if(str_len == 1){ // only has +/- in str
+			exception=-1;
+			return result;
+		}
+	} else if( str[i] == '-'){
+		flag = -1;
+		i++;
+		if(str_len == 1){
+			exception=-1;
+			return result;
+		}
+	}
 
- * */
+	while(i < str_len){
+		if(str[i]>= '0' && str[i]<= '9'){ // non digit
+			if(flag == 1 && (result * 10 + (str[i] - '0'))<= INT_MAX){
+				result = result * 10 + (str[i] - '0');
+				i++;
+			} else if (flag == -1 && flag*(result * 10 + (str[i] - '0'))>= INT_MIN){
+				result = result * 10 + (str[i] - '0');
+				i++;
+			}
+			else {
+				exception = -2;//overflow
+				return result;
+			}
+
+		} else {
+			exception = -1;
+			return result;
+		}
+	}
+	return result*flag;
+}
+
+// output:
+
+//-2147483649
+//overflow!
